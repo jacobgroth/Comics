0c940b4cf21d958ac166b83db6ff605579531b74
jacobgroth <bella2lund>
2016-07-11 20:26:00 +0200

    temp version

--- Datawordreader.py
+++ Datawordreader.py
@@ -3,6 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from Datawordhelperfunctions import *
 
+
 filename="/Users/rgjg/Desktop/6865.2016.0411.0"
 
 f=open(filename)
@@ -11,23 +12,36 @@ lines=f.readlines()
 events = []
 ievt = []
 
+times = []
+
 for i in xrange(1000):
 
     if i+1 >= len(lines): break
 
     dw = timehelperfunc(lines[i])
 
-    ievt.append(i)
+    times.append(dw.getalltimes())
 
     if timehelperfunc(lines[i]).isnewevent():
         print "------------- event nr %s ------------" % i
 
     if timehelperfunc(lines[i+1]).isnewevent():
         events.append(event(len(ievt)))
+
+        listsum = np.zeros(len(times[0]))
+
+        for j in times:
+            listsum += j
+        listsum = listsum.tolist()
+
+        print times
+        print "HEP" , listsum
         ievt = []
+        times = []
 
 
-    print dw.getalltimes()
+
+#    print dw.getalltimes(), lines[i]
 
 
 print len(events)
